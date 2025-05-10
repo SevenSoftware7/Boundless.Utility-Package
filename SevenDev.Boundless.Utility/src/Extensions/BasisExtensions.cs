d31363b31448c6f5d8454dc650e5d06139a0e7b0
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-05-10 17:58:52 +0200

    feat: Add UpTowards method for creating a Basis pointing up towards a target

--- SevenDev.Boundless.Utility/src/Extensions/BasisExtensions.cs
+++ SevenDev.Boundless.Utility/src/Extensions/BasisExtensions.cs
@@ -24,4 +24,33 @@ public static class BasisExtensions {
 	public static Basis FromToBasis(this Basis from, Basis to) {
 		return to * from.Inverse();
 	}
+
+	/// <summary>
+	/// Creates a Godot.Basis with a rotation such that the up axis (+Y) points
+	/// towards the target position. The forward axis (-Z) points as close to the forward vector
+	/// as possible while staying perpendicular to the up axis. The resulting Basis
+	/// is orthonormalized. The target and forward vectors cannot be zero, and cannot be parallel
+	/// to each other.
+	/// </summary>
+	/// <param name="target">The position to point up towards.</param>
+	/// <param name="forward">The relative forward direction.</param>
+	/// <param name="useModelFront">
+	/// If true, then the model is oriented in reverse, towards the model front axis
+	/// (+Z, Vector3.ModelFront), which is more useful for orienting 3D models.
+	/// </param>
+	/// <returns>The resulting basis matrix.</returns>
+	public static Basis UpTowards(Vector3 target, Vector3? forward = null, bool useModelFront = false) {
+		if (!forward.HasValue) {
+			forward = Vector3.Forward;
+		}
+		if (!useModelFront) {
+			forward = -forward;
+		}
+
+		Vector3 up = target.Normalized();
+
+		Vector3 right = up.Cross(forward.Value).Normalized();
+		Vector3 back = right.Cross(up);
+		return new(right, up, back);
+	}
 }
