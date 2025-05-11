2623d38c583c670cb80c1e8c9bb9da16b23149e2
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-05-11 19:28:56 +0200

    feat: Add WarpTowards and WarpUpTowards methods for smoother basis interpolation

--- SevenDev.Boundless.Utility/src/Extensions/BasisExtensions.cs
+++ SevenDev.Boundless.Utility/src/Extensions/BasisExtensions.cs
@@ -53,4 +53,60 @@ public static class BasisExtensions {
 		Vector3 back = right.Cross(up);
 		return new(right, up, back);
 	}
+
+	/// <summary>
+	/// Creates a Godot.Basis that can be easily interpolated to, from the given Basis, without gimble lock.
+	/// The forward axis (+Y) points towards the target position. The up axis (-Z) points as close to the up vector
+	/// as possible while staying perpendicular to the up axis. The resulting Basis
+	/// is orthonormalized. The target and forward vectors cannot be zero, and cannot be parallel
+	/// to each other.
+	/// </summary>
+	/// <param name="basis">The basis to warp.</param>
+	/// <param name="forward">The target forward direction.</param>
+	/// <param name="up">The target up direction.</param>
+	/// <param name="useModelFront">
+	/// If true, then the model is oriented in reverse, towards the model front axis
+	/// (+Z, Vector3.ModelFront), which is more useful for orienting 3D models.
+	/// </param>
+	/// <returns>
+	/// The resulting basis matrix, which can be easily interpolated to, from the given Basis.
+	/// </returns>
+	public static Basis WarpTowards(this Basis basis, Vector3 forward, Vector3? up = null, bool useModelFront = false) {
+		if (!up.HasValue) {
+			up = basis.Up();
+		}
+
+		// Theoretical Rotation, with its -Z = forward and +Y ≈ up
+		Basis targetRotation = Basis.LookingAt(forward, up, useModelFront);
+		// Practical rotation, can be smoothly interpolated to, from the current rotation
+		return FromToBasis(basis, targetRotation) * basis;
+	}
+
+	/// <summary>
+	/// Creates a Godot.Basis that can be easily interpolated to, from the given Basis, without gimble lock.
+	/// The up axis (+Y) points towards the target up. The forward axis (-Z) points as close to the forward vector
+	/// as possible while staying perpendicular to the up axis. The resulting Basis
+	/// is orthonormalized. The target and forward vectors cannot be zero, and cannot be parallel
+	/// to each other.
+	/// </summary>
+	/// <param name="basis">The basis to warp.</param>
+	/// <param name="up">The target up direction.</param>
+	/// <param name="forward">The target forward direction.</param>
+	/// <param name="useModelFront">
+	/// If true, then the model is oriented in reverse, towards the model front axis
+	/// (+Z, Vector3.ModelFront), which is more useful for orienting 3D models.
+	/// </param>
+	/// <returns>
+	/// The resulting basis matrix, which can be easily interpolated to, from the given Basis.
+	/// </returns>
+	public static Basis WarpUpTowards(this Basis basis, Vector3 up, Vector3? forward = null, bool useModelFront = false) {
+		if (!forward.HasValue) {
+			forward = basis.Forward();
+		}
+
+		// Theoretical Rotation, with its +Y = up and -Z ≈ forward
+		Basis targetRotation = UpTowards(up, forward, useModelFront);
+		// Practical rotation, can be smoothly interpolated to, from the current rotation
+		return FromToBasis(basis, targetRotation) * basis;
+	}
 }
