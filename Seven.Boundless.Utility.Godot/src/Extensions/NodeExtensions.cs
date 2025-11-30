30b85d8f1bc6f5ae5fd088c0001400f7a9555754
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-11-30 17:39:03 +0100

    refactor: Rework Node extension methods for easier chaining

--- Seven.Boundless.Utility.Godot/src/Extensions/NodeExtensions.cs
+++ Seven.Boundless.Utility.Godot/src/Extensions/NodeExtensions.cs
@@ -7,115 +7,43 @@ using Godot;
 
 public static class NodeExtensions {
 
-	public static T SafeRename<T>(this T node, StringName name) where T : Node {
-		if (node.Name != name) {
-			node.Name = name;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsEnabled(this Node node) => node.ProcessMode == Node.ProcessModeEnum.Inherit || node.ProcessMode == Node.ProcessModeEnum.Always;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Enabled<T>(this T node, Node.ProcessModeEnum mode = Node.ProcessModeEnum.Inherit) where T : Node {
+		if (node.ProcessMode == Node.ProcessModeEnum.Disabled) {
+			node.ProcessMode = mode;
 		}
 		return node;
 	}
-
-	public static bool IsEnabled(this Node node) => node.ProcessMode == Node.ProcessModeEnum.Inherit || node.ProcessMode == Node.ProcessModeEnum.Always;
-
-	public static void Enable(this Node node) {
-		if (node.ProcessMode == Node.ProcessModeEnum.Disabled) {
-			node.ProcessMode = Node.ProcessModeEnum.Inherit;
-		}
-	}
-	public static void Disable(this Node node) {
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Disabled<T>(this T node) where T : Node {
 		node.ProcessMode = Node.ProcessModeEnum.Disabled;
-	}
-
-	public static void SetEnabled(this Node node, bool enabled) {
-		if (enabled) {
-			node.Enable();
-		}
-		else {
-			node.Disable();
-		}
-	}
-
-	public static void Enable(this Node2D node) {
-		node.Visible = true;
-		if (node.ProcessMode == Node.ProcessModeEnum.Disabled) {
-			node.ProcessMode = Node.ProcessModeEnum.Inherit;
-		}
-	}
-	public static void Disable(this Node2D node) {
-		node.Visible = false;
-		node.ProcessMode = Node.ProcessModeEnum.Disabled;
-	}
-
-	public static void SetEnabled(this Node2D node, bool enabled) {
-		if (enabled) {
-			node.Enable();
-		}
-		else {
-			node.Disable();
-		}
-	}
-
-	public static void Enable(this Node3D node) {
-		node.Visible = true;
-		if (node.ProcessMode == Node.ProcessModeEnum.Disabled) {
-			node.ProcessMode = Node.ProcessModeEnum.Inherit;
-		}
-	}
-	public static void Disable(this Node3D node) {
-		node.Visible = false;
-		node.ProcessMode = Node.ProcessModeEnum.Disabled;
-	}
-
-	public static void SetEnabled(this Node3D node, bool enabled) {
-		if (enabled) {
-			node.Enable();
-		}
-		else {
-			node.Disable();
-		}
-	}
-
-	public static void Enable(this Control node) {
-		node.Visible = true;
-		if (node.ProcessMode == Node.ProcessModeEnum.Disabled) {
-			node.ProcessMode = Node.ProcessModeEnum.Inherit;
-		}
-	}
-	public static void Disable(this Control node) {
-		node.Visible = false;
-		node.ProcessMode = Node.ProcessModeEnum.Disabled;
-	}
-
-	public static void SetEnabled(this Control node, bool enabled) {
-		if (enabled) {
-			node.Enable();
-		}
-		else {
-			node.Disable();
-		}
+		return node;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static T SafeReparentEditor<T>(this T child, Node? newParent, bool keepGlobalTransform = true) where T : Node {
-		child.SafeReparentTo(newParent, keepGlobalTransform);
-
-		if (!Engine.IsEditorHint()) return child;
-		if (child.Owner == newParent?.Owner) return child;
-
-		Reown(child, newParent?.Owner ?? newParent);
-
-		static void Reown(Node childNode, Node? newOwner) {
-			childNode.Owner = newOwner;
-			foreach (Node child in childNode.GetChildren()) {
-				Reown(child, newOwner);
-			}
-		}
-
-		return child;
+	public static T SetEnabled<T>(this T node, bool enabled, Node.ProcessModeEnum mode = Node.ProcessModeEnum.Inherit) where T : Node {
+		if (enabled) return node.Enabled(mode);
+		else return node.Disabled();
 	}
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static T SafeReparentAndSetOwner<T>(this T child, Node? newParent, bool keepGlobalTransform = true) where T : Node {
-		child.SafeReparentTo(newParent, keepGlobalTransform);
-		child.Owner = newParent?.Owner ?? newParent;
+	public static T ToggleEnabled<T>(this T node) where T : Node {
+		if (node.IsEnabled()) return node.Disabled();
+		else return node.Enabled();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T RenameTo<T>(this T node, StringName name) where T : Node {
+		node.Name = name;
+		return node;
+	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T ReparentTo<T>(this T child, Node parent, bool forceReadableName = false) where T : Node {
+		parent.AddChild(child, forceReadableName);
 		return child;
 	}
 
@@ -126,73 +54,45 @@ public static class NodeExtensions {
 		if (!child.IsInsideTree()) {
 			child.Unparent();
 		}
+
 		if (child.GetParent() is null) {
 			newParent?.AddChild(child);
-			return child;
 		}
-
-		if (newParent is not null) {
+		else if (newParent is not null) {
 			child.Reparent(newParent, keepGlobalTransform);
 		}
+
 		return child;
 	}
 
-
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static T SafeReparentAndRename<T>(this T child, Node? newParent, StringName name, bool keepGlobalTransform = true) where T : Node {
-		if (child.GetParent() == newParent) return child;
+	public static T SafeReparentRecursive<T>(this T child, Node? newParent, bool keepGlobalTransform = true) where T : Node {
+		child.SafeReparentTo(newParent, keepGlobalTransform);
 
-		if (!child.IsInsideTree()) {
-			child.Unparent();
+		if (!Engine.IsEditorHint()) return child;
+		if (child.Owner == newParent?.Owner) return child;
+
+		ReownRecursive(child, newParent?.Owner ?? newParent);
+
+		static void ReownRecursive(Node childNode, Node? newOwner) {
+			childNode.Reown(newOwner);
+			foreach (Node child in childNode.GetChildren()) {
+				ReownRecursive(child, newOwner);
+			}
 		}
-		if (child.GetParent() is null) {
-			newParent?.AddChild(child);
-			child.SafeRename(name);
-			return child;
-		}
 
-		if (newParent is not null) {
-			child.Reparent(newParent, keepGlobalTransform);
-			child.SafeRename(name);
-		}
 		return child;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static T AddChildAndSetOwner<T>(this T obj, Node child, bool forceReadableName = false) where T : Node {
-		obj.AddChild(child, forceReadableName);
-		child.Owner = obj.Owner ?? obj;
-		return obj;
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static T AddSceneInstanceChild<T>(this T obj, Node child, bool forceReadableName = false) where T : Node {
-		obj.AddChild(child, forceReadableName);
-		child.Owner = obj.SceneFilePath.Length != 0 ? obj : obj.Owner ?? obj;
-		return obj;
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static TNode ParentTo<TNode>(this TNode child, Node parent, bool forceReadableName = false) where TNode : Node {
-		parent.AddChild(child, forceReadableName);
-		return child;
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static TNode SetOwnerAndParent<TNode>(this TNode child, Node parent, bool forceReadableName = false) where TNode : Node {
-		parent.AddChildAndSetOwner(child, forceReadableName);
-		return child;
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static TNode SetParentToSceneInstance<TNode>(this TNode child, Node parent, bool forceReadableName = false) where TNode : Node {
-		parent.AddSceneInstanceChild(child, forceReadableName);
-		return child;
+	public static T SetSiblingIndex<T>(this T node, Node parent, int index) where T : Node {
+		parent.MoveChild(node, index);
+		return node;
 	}
 
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static TNode Unparent<TNode>(this TNode child) where TNode : Node {
+	public static T Unparent<T>(this T child) where T : Node {
 		child.Owner = null;
 		child.GetParent()?.RemoveChild(child);
 		return child;
@@ -203,6 +103,29 @@ public static class NodeExtensions {
 		obj.QueueFree();
 		obj.GetParent()?.RemoveChild(obj);
 	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T ReparentToSceneInstance<T>(this T child, Node parent, bool forceReadableName = false) where T : Node {
+		parent.AddSceneInstanceChild(child, forceReadableName);
+		return child;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T AddSceneInstanceChild<T>(this T parent, Node child, bool forceReadableName = false) where T : Node {
+		parent.AddChild(child, forceReadableName);
+		child.Owner = parent.SceneFilePath.Length != 0 ? parent : parent.Owner ?? parent;
+		return parent;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Reown<T>(this T node, Node? newOwner) where T : Node {
+		node.Owner = newOwner;
+		return node;
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T ShareOwnerWith<T>(this T node, Node? newParent) where T : Node =>
+		node.Reown(newParent?.Owner ?? newParent);
 
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -284,5 +207,55 @@ public static class NodeExtensions {
 		foreach (Node childNode in node.GetChildren()) {
 			MakeLocal(childNode, owner);
 		}
+	}
+}
+
+public static class Node3DExtensions {
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Visible<T>(this T node) where T : Node3D {
+		node.Visible = true;
+		return node;
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Invisible<T>(this T node) where T : Node3D {
+		node.Visible = false;
+		return node;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T SetVisible<T>(this T node, bool visible) where T : Node3D {
+		if (visible) return node.Visible();
+		else return node.Invisible();
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T ToggleVisible<T>(this T node) where T : Node3D {
+		if (node.Visible) return node.Invisible();
+		else return node.Visible();
+	}
+}
+
+public static class CanvasItemExtensions {
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Visible<T>(this T node) where T : CanvasItem {
+		node.Visible = true;
+		return node;
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T Invisible<T>(this T node) where T : CanvasItem {
+		node.Visible = false;
+		return node;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T SetVisible<T>(this T node, bool visible) where T : CanvasItem {
+		if (visible) return node.Visible();
+		else return node.Invisible();
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static T ToggleVisible<T>(this T node) where T : CanvasItem {
+		if (node.Visible) return node.Invisible();
+		else return node.Visible();
 	}
 }
